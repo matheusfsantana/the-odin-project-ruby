59fd4552bbaac372b71739b354d2e02aa82e3466
matheusfsantana <matheusfsantana99@gmail.com>
2024-05-29 01:04:33 -0300

    refactor with rubocop

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,34 +1,35 @@
 def caesar_cipher(string, shift)
-    letters = []
-    string.each_char {|char| letters << char.ord}
-
-    # a-z = 97-122
-    # A-Z = 65-90
-    letters.map! do |letter| 
-        if letter.between?(97,122)
-            # Caesar cipher formula -> En(x) = (x + n) mod 26
-            # Adapted formula -> En(x) = (x - 97 + n) mod 26 + 97
-            letter = ((letter - 97 + shift) % 26 + 97 ).chr
-        elsif letter.between?(65,90)
-            # Caesar cipher formula -> En(x) = (x + n) mod 26
-            # Adapted formula -> En(x) = (x - 65 + n) mod 26 + 65
-            letter = ((letter - 65 + shift) % 26 + 65).chr
-        else
-            letter = letter.chr
-        end
+  string.chars.map do |char|
+    if char.between?("a", "z")
+      lowercase_shift(char, shift)
+    elsif char.between?("A", "Z")
+      uppercase_shift(char, shift)
+    else
+      char
     end
-    letters.join
+  end.join
+end
+
+# Caesar cipher formula -> En(x) = (x + n) mod 26
+# Adapted formula -> En(x) = (x - 97 + n) mod 26 + 97 ~ [97 is the numeric value of character]
+def lowercase_shift(char, shift)
+  (((char.ord - "a".ord + shift) % 26) + "a".ord).chr
+end
+
+# Caesar cipher formula -> En(x) = (x + n) mod 26
+# Adapted formula -> En(x) = (x - 65 + n) mod 26 + 65 ~ [65 is the numeric value of character]
+def uppercase_shift(char, shift)
+  (((char.ord - "A".ord + shift) % 26) + "A".ord).chr
 end
 
 def main
+  p "Enter the text to be encrypted: "
+  plaintext = gets.chomp
+  p "Enter a shift value"
+  shift = gets.chomp.to_i
+  ciphertext = caesar_cipher(plaintext, shift)
 
-    p "Enter the text to be encrypted: "
-    plaintext = gets.chomp
-    p "Enter a shift value"
-    shift = gets.chomp.to_i
-    ciphertext = caesar_cipher(plaintext, shift)
-
-    puts "Plaintext: #{plaintext}\nCiphertext: #{ciphertext}"
+  puts "Plaintext: #{plaintext}\nCiphertext: #{ciphertext}"
 end
 
 main
