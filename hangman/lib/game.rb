6da35984225c7244c5dbfbdc036003097d9e108a
matheusfsantana <matheusfsantana99@gmail.com>
2024-06-18 21:46:33 -0300

    fixes from rubocop suggestions

--- hangman/lib/game.rb
+++ hangman/lib/game.rb
@@ -2,11 +2,11 @@ class Game
   WORDS_FILENAME = 'words_file.txt'.freeze
   attr_accessor :secret, :guess, :attempts, :wrong_guesses
 
-  def initialize(secret=self.generate_secret, guess=[], attempts=7, wrong_guesses=[])
+  def initialize(secret = generate_secret, guess = [], attempts = 7, wrong_guesses = [])
     # This logic is because the game can be loaded or started from 0
     if guess.empty?
       secret.length.times do
-        guess << "-"
+        guess << '-'
       end
     end
 
@@ -22,31 +22,27 @@ class Game
 
   def generate_secret
     if File.exist? WORDS_FILENAME
-        words = File.new(WORDS_FILENAME, 'r').map do |word| 
-            word
-        end
-        secret = words.sample
-        
-        # Loop to ensure that the secret is always be between 5~12 letters
-        until secret.length.between?(5,12)
-          secret = words.sample
-        end
-        secret.chomp.split('')
-    else
-        puts "File '#{WORDS_FILENAME}' not found"
-    end
+      words = File.new(WORDS_FILENAME, 'r').map do |word|
+        word
+      end
+      secret = words.sample
 
-   
+      # Loop to ensure that the secret is always be between 5~12 letters
+      secret = words.sample until secret.length.between?(5, 12)
+      secret.chomp.chars
+    else
+      puts "File '#{WORDS_FILENAME}' not found"
+    end
   end
 
   def feedback
-    if self.win?
-      puts "Congratulations! The secret is '#{@secret.join("")}'"
-    elsif self.lose?
-      puts "You lose! The secret is '#{@secret.join("")}'"
+    if win?
+      puts "Congratulations! The secret is '#{@secret.join}'"
+    elsif lose?
+      puts "You lose! The secret is '#{@secret.join}'"
     else
       puts "Remaining attempts: #{@attempts}"
-      puts "Wrong guesses: #{@wrong_guesses.join("-")}"
+      puts "Wrong guesses: #{@wrong_guesses.join('-')}"
     end
   end
 
@@ -55,33 +51,33 @@ class Game
   end
 
   def lose?
-    self.attempts == 0
+    @attempts.zero?
   end
 
   def user_guess
-    guess = ""
-    until guess.length > 0
-      puts "Type a letter: "
+    guess = ''
+    until guess.length == 1
+      puts 'Type a letter: '
       guess = gets.chomp
-      puts "Value not allowed" if guess.length == 0
+      puts 'Value not allowed' if guess.length > 1
     end
     guess
   end
 
   def start_game
-    until self.win? || self.lose?
-      puts @guess.join("")
-      guess = self.user_guess
-      self.check_guess(guess)
-      self.feedback
+    until win? || lose?
+      puts @guess.join
+      guess = user_guess
+      check_guess(guess)
+      feedback
     end
   end
-  
-  # Check the guess, and if the guess is ok, put the letter in the correct place, otherwise decrease the attempts and add the letter to wrong_guesses array
+
+  # Check the guess, and if the guess is ok, put the letter in the correct place
+  # Otherwise decrease the attempts and add the letter to wrong_guesses array
   def check_guess(guess)
-    p guess
     indexes = @secret.each_index.select { |index| @secret[index] == guess }
-    if indexes.length > 0
+    if indexes.length.positive?
       indexes.each do |index|
         @guess[index] = guess
       end
@@ -91,7 +87,5 @@ class Game
     end
   end
 
-  def check_saved_games
-   
-  end
+  def check_saved_games; end
 end
